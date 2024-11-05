b78ab58c63502e8e9d677290b6be7d5a7c74d2b7
Aditya733101 <bittusarkar733101@gmail.com>
2024-11-05 20:34:47 +0530

    string and function

--- string.php
+++ string.php
@@ -6,5 +6,12 @@ echo strrev($var);// reverse of string
 echo "<br>";
 echo strpos($var,"sar");//position of string
 echo "<br>";
+
+$num= 34;
+$var1 = 89;
+add($num,$var1);
+function add($num,$var1){
+  echo $num +$var1;
+}
  
 ?>
