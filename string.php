285159045c6e904c7804eb5bdb34ac27eabf07bf
Aditya733101 <bittusarkar733101@gmail.com>
2024-11-05 20:27:46 +0530

    string

---
+++ string.php
@@ -0,0 +1,9 @@
+<?php
+$var = "ADitya sarrkar";
+echo strlen($var);//length of string    
+echo "<br>";
+echo strrev($var);// reverse of string
+echo "<br>";
+echo strpos($var,"sar");//position of string
+echo "<br>";
+?>
